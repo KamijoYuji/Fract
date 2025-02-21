bcb27dbce30574c0f7669e2fac095b1cba5b2555
KamijoYuji <130177873+KamijoYuji@users.noreply.github.com>
2025-02-21 09:59:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@ class Fract {
 	double b;
 	double value = 0.0;
 public:
+	static int gcd(int x, int y) {
+		if (!x or !y)
+			return x + y;
+		if (x >= y)
+			return gcd(x % y, y);
+		if (x <= y)
+			return gcd(x, y % x);
+	}
+
 	void reduct(double eps = 0.00001) {
 		if (!a and b) {
 			b = 1;
@@ -20,19 +29,13 @@ public:
 		bool aflag = a < 0; a = abs(a);
 		bool bflag = b < 0; b = abs(b);
 
-		while (!(abs(floor((a+0.5)) - a) < eps and abs(floor((b + 0.5)) - b) < eps)) {
+		while (!(abs(floor((a + 0.5)) - a) < eps and abs(floor((b + 0.5)) - b) < eps)) {
 			a *= 10; b *= 10;
 		}
 
-		int c = a < b ? a : b;
-		while (c) {
-			if (!(int(float(a)) % c) and !(int(float(b)) % c))
-			{
+		int c = gcd(int(float(a)), int(float(b)));
 				a /= double(c);
 				b /= double(c);
-			}
-			c--;
-		}
 
 		if (!(aflag and bflag)) {
 			a = aflag ? -a : a;
@@ -57,43 +60,95 @@ public:
 				out << other.a;
 			else
 				if (!other.a)
-				out << "0";
+					out << "0";
 				else
 					out << other.a << "/" << other.b;
 		return out;
 	}
 
-		string getVal() {return (b ? to_string(value) : "ERROR"); }
-
-		Fract operator + (Fract& other) {
-			Fract c = Fract(a * other.b + other.a * b, b * other.b);
-			c.reduct();
-			return c;
+	friend istream& operator >> (istream& in, Fract& other) {
+		string val;
+		cin >> val;
+		string a_;
+		int q = 0;
+		while (val[q] != '/' and val[q]) {
+			if (!((val[q] >= '0' and val[q] <= '9') or val[q] == '-' or val[q] == '.'))
+				return in;
+			a_ += val[q];
+			q++;
 		}
-
-		Fract operator - (Fract& other) {
-			Fract c = Fract(a * other.b - other.a * b, b * other.b);
-			c.reduct();
-			return c;
+		if (a_ == val)
+			other = Fract(stod(a_));
+		else
+		{
+			int i = a_.size() + 1;
+			string b_;
+			for (int j = val.size(); i < j; i++)
+				b_ += val[i];
+			other = Fract(stod(a_), stod(b_));
 		}
+		return in;
+	}
 
-		Fract operator * (Fract& other) {
-			Fract c = Fract(a * other.a, b * other.b);
-			c.reduct();
-			return c;
-		}
+	string getVal() { return (b ? to_string(value) : "ERROR"); }
 
-		Fract operator / (Fract& other) {
-			Fract c = Fract(a * other.b, b * other.a);
-			c.reduct();
-			return c;
-		}
+	Fract operator + (Fract& other) {
+		Fract c = Fract(a * other.b + other.a * b, b * other.b);
+		return c;
+	}
+
+	Fract operator - (Fract& other) {
+		Fract c = Fract(a * other.b - other.a * b, b * other.b);
+		return c;
+	}
+
+	Fract operator * (Fract& other) {
+		Fract c = Fract(a * other.a, b * other.b);
+		return c;
+	}
+
+	Fract operator / (Fract& other) {
+		Fract c = Fract(a * other.b, b * other.a);
+		return c;
+	}
 };
 
+pair<int, int> func(int a, int b) {
+	int q = 1;
+	int x1 = 1;
+	int x2 = 0;
+	int y1 = 0;
+	int y2 = 1;
+	int i = 0;
+	if (a > b) {
+		int s = a;
+		a = b;
+		b = s;
+	}
+	int f;
+	while (a and b) {
+		if (!(i % 2)) {
+			q = a / b;
+			a = a % b;
+			x1 -= q*x2;
+			y1 -= q*y2;
+			cout << q << " " << a << " " << x1 << " " << y1 << endl;
+		}
+		else {
+			q = b / a;
+			b = b % a;
+			x2 -= q*x1;
+			y2 -= q*y1;
+			cout << q << " " << b << " " << x2 << " " << y2 << endl;
+		}
+		i++;
+	}
+	if (a)
+		return pair<int,int>(y1,x1);
+	return pair<int,int>(y2,x2);
+}
+
 int main() {
-	Fract a(-52.4,-3.012);
-	Fract b(-3.157);
-	Fract c = a * b;
-	cout << c;
+	
 	return 0;
 }
